436b18c96ac1fafa158a2772cc1900fbb27c8b15
Larbi Kouiti <larbi@zidoworld.com>
2020-01-22 23:33:24 +0100

    end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
+// State vs Props
+// Class Component
+// Functional Component
+
 function App() {
   return (
     <div>
-      Hello World!
+      Hello World! <br/>
+      <Person name={'larbi'} /> <br/>
+      <PersonClass name={'azzeddine'} />
     </div>
   );
+}
+
+const Person = ({name}) => {
+  React.useEffect( () => {
+    // code goes here
+  }, []);
+  return <span>My name is: {name}</span> 
+};
+
+class PersonClass extends React.Component {
+
+
+  componentDidMount() {
+    // code goes here
+  }
+
+  render() {
+    const {name} = this.props;
+    return <span>My name is: {name}</span>;
+  }
 }
 
 export default App;
