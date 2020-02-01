073ec0441365c93c6882e572c76c027b86d5e506
Larbi Kouiti <larbi@zidoworld.com>
2020-02-01 12:01:57 +0100

    - adding form and list together in the application - define state flag swicher to show each interface when needed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 function App() {
+
+  const [currentInterface, setCurrentInterface] = React.useState('form');
+
   const [formData, setFormData] = React.useState({
     emitter: '',
     text: '',
     status: 'unread', // Should be in BE
   });
 
-  const [loading, setLoading] = React.useState( false );
+  const [loading, setLoading] = React.useState(false);
 
   const handleChange = (name) => (evt) => {
     setFormData({
@@ -19,7 +22,7 @@ function App() {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    setLoading( true );
+    setLoading(true);
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -27,53 +30,82 @@ function App() {
     fetch('http://localhost:3030/notifications',
       {
         method: 'POST',
-        body: JSON.stringify( formData ),
+        body: JSON.stringify(formData),
         headers: myHeaders,
       }
-    ).then( (resp) => resp.json() )
-    .then( (result) => {
-      setFormData({
-        emitter: '',
-        text: '',
-        status: 'unread', // Should be in BE
+    ).then((resp) => resp.json())
+      .then((result) => {
+        setFormData({
+          emitter: '',
+          text: '',
+          status: 'unread', // Should be in BE
+        })
+        setLoading(false)
       })
-      setLoading(false)
-      console.log( result );
-      debugger;
-    })
   }
 
   return (
     <div className="container">
       <h2>Notifcations</h2>
-      <h5>Creator Form</h5>
-      { loading && <p style={{color: 'orangered'}}>Your notification is being processed, please wait...</p> }
-      { 
-        !loading && 
+      {
+        currentInterface === 'list' &&
         <div>
-          <p>The following form allows you to create new notifications</p>
-          <form onSubmit={handleSubmit}>
-            <label>Notifcation Emitter</label>
-            <select
-              disabled={loading}
-              value={formData.emitter}
-              className="u-full-width"
-              required
-              onChange={handleChange( 'emitter' )}
-            >
-              <option></option>
-              <option>Azzeddine</option>
-              <option>Larbi</option>
-            </select>
-            <label>Notification Text</label>
-            <textarea
-              required
-              disabled={loading}
-              value={formData.text} 
-              onChange={handleChange('text')}
-              className="u-full-width" />
-            <button type="submit" disabled={loading} className="u-pull-right">Sumbit</button>
-          </form>
+          <h5>List of Notifications</h5>
+          <p>This is the list of notification existing in the datatabse</p>
+          <table class="u-full-width">
+            <thead>
+              <tr>
+                <th>Text</th>
+                <th>Emitter</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Lorem Ipsum</td>
+                <td>Azzeddine</td>
+              </tr>
+              <tr>
+                <td>Lorem Ipsum</td>
+                <td>Larbi</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      }
+
+      {
+        currentInterface === 'form' &&
+        <div>
+          <h5>Creator Form</h5>
+          {loading && <p style={{ color: 'orangered' }}>Your notification is being processed, please wait...</p>}
+          {
+            !loading &&
+            <div>
+              <p>The following form allows you to create new notifications</p>
+              <form onSubmit={handleSubmit}>
+                <label>Notifcation Emitter</label>
+                <select
+                  disabled={loading}
+                  value={formData.emitter}
+                  className="u-full-width"
+                  required
+                  onChange={handleChange('emitter')}
+                >
+                  <option></option>
+                  <option>Azzeddine</option>
+                  <option>Larbi</option>
+                </select>
+                <label>Notification Text</label>
+                <textarea
+                  required
+                  disabled={loading}
+                  value={formData.text}
+                  onChange={handleChange('text')}
+                  className="u-full-width" />
+                <button type="submit" disabled={loading} className="u-pull-right">Sumbit</button>
+              </form>
+            </div>
+          }
         </div>
       }
     </div>
