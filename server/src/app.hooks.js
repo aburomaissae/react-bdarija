f8b330b8f7b750b5bb9ba6d3d6c1fbf543a26be1
Larbi Kouiti <larbi@zidoworld.com>
2020-02-01 11:46:38 +0100

    disable pagination

--- server/src/app.hooks.js
+++ server/src/app.hooks.js
@@ -1,9 +1,14 @@
 // Application hooks that run for every service
+const noPaging = (context) => {
+  context.params.paginate = false;
+  delete context.params.query.$limit;
+  return context;
+};
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [noPaging],
     get: [],
     create: [],
     update: [],
